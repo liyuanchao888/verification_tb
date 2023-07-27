1635f273cc34f8b83ff4e5f846d591a915ccfec7
liycstart888 <liycstart@126.com>
2023-07-27 02:40:45 +0000

    modify coverage.sh to satisfy the -f merge_cov.fl

--- xt_demo/7_run/1_script/coverage.sh
+++ xt_demo/7_run/1_script/coverage.sh
@@ -1,46 +1,53 @@
 #!/usr/bin/bash
 #------ build TC directory ------
-if [ ! -d ${PROJ_WORK_PATH}/${tc} ];then
+if [[ ! -d ${PROJ_WORK_PATH}/${tc} ]];then
 	mkdir -p ${PROJ_WORK_PATH}/${tc}
 else
 	echo " ${PROJ_WORK_PATH}/${tc} is exist "
 fi
 
-
 #------- option from command line ---------
-if [ $# == 0 ];then
-    echo " run_sim.sh no self-define para from command line "
+#if [ $# == 0 ];then
+#    echo " no self-define para to coverage script "
+#else
+#    COV_CMD_DEF=$*
+#fi
+
+
+#------- view coverage report file ----------
+if [[ -n "${report}" ]];then
+    if [ ${report} = "dve" ];then
+	    COV_OPT="dve -full64 -cov -logdir ${PROJ_WORK_PATH}/cov "
+	else
+    	COV_OPT="firefox ${PROJ_WORK_PATH}/cov/urgReport/dashboard.html "
+	fi
+#------- merge coverage db file ----------
 else
-    COV_CMD_DEF=$*
+    COV_OPT="urg \
+        "
+    COV_OPT="${COV_OPT} -full64 -parallel -maxjobs {paral_num} "
+    
+	echo -e " add coverage db file to ${PROJ_WORK_PATH}/cov/merge_vdb.fl to merge "
+    if [[ ! -d "${PROJ_WORK_PATH}/cov/merge_vdb.fl" ]];then
+        echo -e "${PROJ_WORK_PATH}/cov/simv.vdb" > ${PROJ_WORK_PATH}/cov/merge_vdb.fl
+    fi
+    COV_OPT="${COV_OPT} -f ${PROJ_WORK_PATH}/cov/merge_vdb.fl "
+    COV_OPT="${COV_OPT} -report ${PROJ_WORK_PATH}/cov/urgReport  -dbname ${PROJ_WORK_PATH}/cov/merge_cov/merge"
 fi
 
-COV_OPT="urg \
-    "
-#    COV_OPT="${COV_OPT} ${COV_CMD_DEF} "
 
-#====== ADD part 1: vcs switch on-off configuration       =======
-#---- code coverage  -----
-COV_OPT="${COV_OPT} -full64 -parallel -maxjobs {paral_num} -dir ${PROJ_WORK_PATH}/cov/*simv.vdb "
-#------- merge file from file_path ----------
-if [ -n "${opt}" ]; then
+if [[ -n "${opt}" ]]; then
 	ACT_PARA=`echo ${opt} | sed 's/--/ /g'`
     for file_path in ${ACT_PARA} 
     do
     	COV_OPT="${COV_OPT} -dir ${file_path} "
     done
+else
+	COV_OPT="${COV_OPT} -dir ${PROJ_WORK_PATH}/cov/*simv.vdb "
 fi
 
-COV_OPT="${COV_OPT} -report ${PROJ_WORK_PATH}/cov/urgReport  -dbname ${PROJ_WORK_PATH}/cov/merge_cov/merge"
-
-if [ -n "${report}" ];then
-    if [ ${report} = "dve" ];then
-	    COV_OPT="dve -full64 -dir ${PROJ_WORK_PATH}/cov/*simv.vdb -logdir ${PROJ_WORK_PATH}/cov & "
-	else
-    	COV_OPT="firefox ${PROJ_WORK_PATH}/cov/urgReport/dashboard.html & "
-	fi
-fi
-
-echo -e "coverage urg command is ${COV_OPT}"
+COV_OPT=" ${COV_OPT} & "
+echo -e "coverage urg command : ${COV_OPT}"
 eval ${COV_OPT}
 
 
