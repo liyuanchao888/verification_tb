c37e1b0323c82686f8be979c610511b8dc228ca9
liycstart888 <liycstart@126.com>
2023-11-14 12:19:59 +0000

    update the rerun Makefile command

--- xt_demo/7_run/1_script/common.mk
+++ xt_demo/7_run/1_script/common.mk
@@ -6,16 +6,16 @@ default: help
 help:
 	@bash help.sh 
 
-get_dut:
+get_dut:record
 	@bash ${GET_FILELIST}
 
-gen_run:
+gen_run:record
 	@bash ${GEN_RUN_FILE} ${CFG_ALL}
 
-comp:
+comp:record
 	@bash ${RUN_COMPILE_FILE}
 
-sim:
+sim:record
 	@bash ${RUN_SIM_FILE}
 
 ifeq (${gcs},s)
@@ -26,16 +26,16 @@ else
 run:gen_run comp sim
 endif
 
-wave:
+wave:record
 	@bash ${RUN_WAVE_FILE}
 
 regr:gen_run
 	@bash ${RUN_REGR_FILE} ${opt}
 
-report:
+report:record
 	@bash ${RUN_REPORT_FILE}
 
-cov:
+cov:record
 	@bash coverage.sh ${opt} ${report}
 
 clean:
@@ -44,3 +44,10 @@ clean:
 tc:
 	@find ${TB_PATH}/6_top/2_tc -name "*.sv" -o -name "*.v"
 
+ifneq (${MAKECMDGOALS},)#no null
+ifeq ($(filter tc clean help,${MAKECMDGOALS}),)#no tc clean,help 
+record:
+	@echo "make ${MAKECMDGOALS} ${MAKEFLAGS}" >rerun
+	chmod +x rerun
+endif
+endif
